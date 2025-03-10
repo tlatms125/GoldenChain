8f368e5cef9597e3a84f0692cb332c54562a9bc4
SIN <dkrp12@gmail.com>
2025-03-10 19:02:55 +0900

    백업

---
+++ Assets/Script/NPC.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //NPC를 설정하고 플레이어가 NPC 근처에 가서 F키를 누르면 상호작용하기라는 로그를 출력해주세요.
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+    public Rigidbody2D rgby;
+    
+    void Start()
+    {
+        rgby = GetComponent<Rigidbody2D>();
+    }
+    public float moveSpeed = 2; 
+    void Update()
+    {
+        Vector2 direction = new Vector2(0,0); // Vector2.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += new Vector2(0,1);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            direction += new Vector2(0,-1);
+        }
+         if(Input.GetKey(KeyCode.A))
+        {
+            direction += new Vector2(-1,0);
+        }
+         if(Input.GetKey(KeyCode.D))
+        {
+            direction += new Vector2(1,0);
+        }
+        rgby.linearVelocity = direction.normalized * moveSpeed;
+       
+    }
+    //NPC?? ??????? ?��???? NPC ????? ???? F??? ?????? ??????????? ?��?? ??????????.
+    
+}

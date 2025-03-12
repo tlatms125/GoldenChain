33cd727bb4b01b463ee0dc3d47cc8bb3004698ad
SIN <dkrp12@gmail.com>
2025-03-13 00:32:03 +0900

    서밋

--- Assets/1.Script/DialogueManager.cs
+++ Assets/1.Script/DialogueManager.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-
+using TMPro;
 public class DialogueManager : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
